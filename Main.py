217d2d642f4fdc3bb953923cbee654936e92d214
MaorShtern <93667425+MaorShtern@users.noreply.github.com>
2022-06-26 12:39:17 +0300

    conect to SQL

--- Main.py
+++ Main.py
@@ -1,5 +1,23 @@
+import pyodbc
+
+
+# Connecting to SQL and returning the Cursor so we can work on the database
+def Connect_to_SQL_Server():
+    server = 'sql5108.site4now.net'
+    database = 'db_a79b5b_proj13'
+    username = 'db_a79b5b_proj13_admin'
+    password = 'XXNEA6q6VbvATG6g'
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    return cursor
+
+
 def main():
-    print()
+    cursor = Connect_to_SQL_Server()
+    cursor.execute('exec GetAllEmployees')
+    for i in cursor:
+        print(i)
 
 
 if __name__ == '__main__':
