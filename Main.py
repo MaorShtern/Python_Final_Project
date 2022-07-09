862df890c74df279b4ffe1d1dbf07a120c5db57c
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-07-09 10:40:11 +0300

    #

--- Main.py
+++ Main.py
@@ -1,32 +1,26 @@
 from Customers_Rooms import Month_with_the_most_reservation
 from Tasks import Number_of_tasks_per_month
 
-
 import Products
 import Financial
 
 
-
-
-
 def main():
-<<<<<<< Updated upstream
-    #Number_of_tasks_per_month()
-    #Month_with_the_most_reservation()
+    # Number_of_tasks_per_month()
+    # Month_with_the_most_reservation()
     # Products.get_product_sales()
     # Products.most_products_sales_in_each_category()
     # product_code = input("Product_Purchase_By_Code: ")
     # Products.Product_Purchase_By_Code(product_code=product_code)
     Financial.Rooms_Income()
-    Financial.Purchase_Of_Goods_Expenses()
-    Financial.Products_Income()
-=======
+    # Financial.Purchase_Of_Goods_Expenses()
+    # Financial.Products_Income()
+
     # Number_of_tasks_per_month()
     # Month_with_the_most_reservation()
 
-   #Products.get_product_sales()
-    Products.most_products_sales_in_each_category()
->>>>>>> Stashed changes
+    # Products.get_product_sales()
+    # Products.most_products_sales_in_each_category()
 
 
 if __name__ == '__main__':
